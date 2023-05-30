28aa396feea1d3c25252299745222e453532b508
Rakka Rage <rakkarage@gmail.com>
2023-05-30 18:17:38 -0400

    x

---
+++ Test.gd
@@ -0,0 +1,26 @@
+extends TileMap
+
+enum Layer {
+	Back,
+	Fore,
+	Flower,
+	WaterBack, SplitBack, ItemBack,
+	Tree,
+	ItemFore, SplitFore, WaterFore,
+	Top,
+	Light,
+	Edge
+}
+
+enum Tile {
+	Cliff,
+	Doodad,
+	Edge,
+	Light,
+	LightDebug,
+	Outside,
+	Rubble
+}
+
+func _ready():
+	pass
